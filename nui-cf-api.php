909fc9f3709404bb4961ab23ca96e93df48b7d22
ngxson <thichthat@gmail.com>
2019-11-03 16:04:57 +0100

    remove x-frame header

--- nui-cf-api.php
+++ nui-cf-api.php
@@ -3,14 +3,12 @@
  * Plugin Name: Chatfuel Woocommerce
  * Plugin URI: https://ngxson.com
  * Description: Chatfuel Woocommerce JSON API
- * Version: 1.2
+ * Version: 1.4
  * Author: ngxson
  * Author URI: https://ngxson.com
  */
 
 header("Access-Control-Allow-Origin: *");
-header("X-Frame-Options: ALLOW-FROM https://www.messenger.com/");
-header("X-Frame-Options: ALLOW-FROM https://www.facebook.com/");
 
 include __DIR__ . '/lib_utils.php';
 include __DIR__ . '/action_self_update.php';
@@ -22,7 +20,7 @@ include __DIR__ . '/page_settings.php';
 /**
  * Allow rendering of checkout and account pages in iframes.
  */
-add_action( 'after_setup_theme', 'wc_remove_frame_options_header', 11 );
+add_action( 'after_setup_theme', 'wc_remove_frame_options_header', 999 );
 function wc_remove_frame_options_header() {
 	remove_action( 'template_redirect', 'wc_send_frame_options_header' );
 }
