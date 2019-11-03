9b0518d2836b6deabf6c7cf4702597958404162e
ngxson <thichthat@gmail.com>
2019-11-03 15:23:10 +0100

    allow iframe

--- nui-cf-api.php
+++ nui-cf-api.php
@@ -14,3 +14,16 @@ include __DIR__ . '/action_wh_order_create.php';
 include __DIR__ . '/api_last_order.php';
 include __DIR__ . '/api_user.php';
 include __DIR__ . '/page_settings.php';
+
+/**
+ * Allow rendering of checkout and account pages in iframes.
+ */
+add_action( 'after_setup_theme', 'wc_remove_frame_options_header', 11 );
+function wc_remove_frame_options_header() {
+	remove_action( 'template_redirect', 'wc_send_frame_options_header' );
+}
+
+function add_cors_http_header(){
+    header("Access-Control-Allow-Origin: *");
+}
+add_action('init','add_cors_http_header');
